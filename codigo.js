c78e0a86af52a06a083715d617f31b0bb327aca0
antocape <114458997+antocape@users.noreply.github.com>
2022-10-10 12:36:29 -0300

    Add files via upload

---
+++ codigo.js
@@ -0,0 +1,15 @@
+let numeroUno;
+let numeroDos;
+    
+const TITULO = "PRACTICA_DE_VARIABLES";
+const A = "CONTENEDOR";
+const B = "RESULTADO";
+var booleanaUno;
+var booleanaDos;
+var nombreAlumno;
+    nombreAlumno = "Antonella_Cappelletti";
+    console.log('nombreAlumno')
+
+var comision;
+    comision = 22568;
+    console.log('comision')
